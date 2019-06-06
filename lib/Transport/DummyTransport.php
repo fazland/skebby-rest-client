2ce3f63ba544bab5ae8bb6991d8183fc2881cf72
Massimiliano Arione <garakkio@gmail.com>
2019-06-06 16:21:01 +0200

    add NullTransport (#10)
    Add DummyTransport

---
+++ lib/Transport/DummyTransport.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace Fazland\SkebbyRestClient\Transport;
+
+/**
+ * A dummy Transport that always returns the same response.
+ * Useful for testing purposes.
+ *
+ * @author Massimiliano Arione <garakkio@gmail.com>
+ */
+class DummyTransport implements TransportInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function executeRequest(string $uri, string $body): string
+    {
+        return '<?xml version="1.0" encoding="UTF-8"?>
+<SkebbyApi_Public_Send_SmsEasy_Advanced generator="zend" version="1.0">
+<test_send_sms_classic_report><remaining_sms>5</remaining_sms><id>1477056680</id><status>success</status></test_send_sms_classic_report>
+</SkebbyApi_Public_Send_SmsEasy_Advanced>';
+    }
+}
